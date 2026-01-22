fe2c100e0dd80ae965b4d2c78e68e9501bb68e3d
Niranjan Ganesan <niranjaninduja006@gmail.com>
2026-01-22 20:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
 
